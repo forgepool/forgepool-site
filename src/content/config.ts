d6f16dc287deb02ddb3c17bb8c0966e9d531d944
se3stn <sebastian.moy@t-online.de>
2026-07-16 23:02:57 +0200

    Add stable article ID to blog schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,7 @@ import { defineCollection, z } from "astro:content";
 const blog = defineCollection({
   type: "content",
   schema: z.object({
+    articleId: z.string().uuid(),
     title: z.string(),
     description: z.string(),
     date: z.coerce.date(),
